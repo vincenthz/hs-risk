5c56a53a6ba7c0bc8fca8544ca9db596043c3572
Vincent Hanquez <vhanquez@pivotmail.com>
2015-01-29 03:31:47 +0000

    export some types

--- Risk/Types.hs
+++ Risk/Types.hs
@@ -1,3 +1,13 @@
 module Risk.Types
-    (
+    ( Player(..)
+    , Units
     ) where
+
+type Units = Int
+
+data Player =
+      PlayerRed
+    | PlayerGreen
+    | PlayerBlue
+    | PlayerYellow
+    deriving (Show,Eq,Enum)
