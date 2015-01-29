690e9a7a9543ef1d66138670da708ebcc1abd110
Nicolas DI PRIMA <nicolas@di-prima.fr>
2015-01-29 03:53:16 +0000

    Battle: use units from Risk.Types

--- Risk/Battle.hs
+++ Risk/Battle.hs
@@ -6,30 +6,30 @@ import Control.Applicative
 import Control.Monad
 import Data.List (sort)
 import System.Random
-import Risk.Army
+import Risk.Types
 
-runBattle :: Army
-          -> Army
-          -> IO (Army, Army)
+runBattle :: Units
+          -> Units
+          -> IO (Units, Units)
 runBattle offensive defensive = do
     offDices <- getDices offensive 3
     defDices <- getDices defensive 2
     return $ runCombats (offensive, defensive) $ zip offDices defDices
 
-runCombats :: (Army, Army)
+runCombats :: (Units, Units)
            -> [(Int, Int)]
-           -> (Army, Army)
+           -> (Units, Units)
 runCombats armies [] = armies
 runCombats (offensive, defensive) ((doff, ddef):xs) =
     if (doff > ddef)
-        then runCombats (offensive, decrementArmy defensive) xs
-        else runCombats (decrementArmy offensive, defensive) xs
+        then runCombats (offensive    , defensive - 1) xs
+        else runCombats (offensive - 1, defensive    ) xs
 
-getDices :: Army
+getDices :: Units
          -> Int
          -> IO [Int]
 getDices army maxDices =
     reverse . sort <$> replicateM size (randomRIO (1, 7))
   where
     size :: Int
-    size = min (getArmySize army) maxDices
+    size = min army maxDices
